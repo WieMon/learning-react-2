ba7404bd160c0ad807ee0abb993da6d958cf9158
Monika Wieczorek <monikawieczoreq@gmail.com>
2020-04-29 15:12:44 +0200

    Add submission

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -8,20 +8,33 @@ class App extends React.Component {
 
   state = {
     isConfirmed: false,
+    isFormSubmitted: false
   }
 
   handleCheckboxChange = () => {
     this.setState({
-      isConfirmed: !this.state.isConfirmed
+      isConfirmed: !this.state.isConfirmed,
+      isFormSubmitted: false
     })
   }
 
-  displayMessage = () => {
-    if(this.state.isConfirmed) {
-      return <PositiveMessage />
-    } else {
-      return <NegativeMessage />
+  handleFormSubmit = (e) => {
+    e.preventDefault()
+    if(!this.state.isFormSubmitted) {
+      this.setState({
+        isFormSubmitted: true
+      })
     }
+  }
+
+  displayMessage = () => {
+    if(this.state.isFormSubmitted){
+      if(this.state.isConfirmed) {
+        return <PositiveMessage />
+      } else {
+        return <NegativeMessage />
+      }
+    } else {return null}
   }
 
   render() {
@@ -29,11 +42,15 @@ class App extends React.Component {
     return (
       <div>
         <h1>Buy the ticket!</h1>
-        <input type='checkbox' id='age' 
-        onChange={this.handleCheckboxChange} 
-        checked={this.state.isConfirmed} />
-        <label htmlFor='age'>I am 18 years old</label>
-        {this.displayMessage()} 
+        <form onSubmit={this.handleFormSubmit}>
+          <input type='checkbox' id='age' 
+                 onChange={this.handleCheckboxChange} 
+                 checked={this.state.isConfirmed} />
+          <label htmlFor='age'>I am 18 years old</label>
+          <br/>
+          <button type='submit'>Buy</button>
+          {this.displayMessage()} 
+        </form>    
       </div>
     )
   }
